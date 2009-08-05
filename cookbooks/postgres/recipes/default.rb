985e862f5a9fb75905df8333146a523cdf08969c
Corey Donohoe <atmos@atmos.org>
2009-08-05 12:55:37 -0600

    couple of changes to keep postgres data on /db and symlink off to it when it's being initialized

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -3,48 +3,84 @@ require 'pp'
 # Cookbook Name:: postgres
 # Recipe:: default
 #
+postgres_root    = '/var/lib/postgresql'
+postgres_version = '8.3'
+
+directory '/db/postgresql' do
+  owner 'postgres'
+  group 'postgres'
+  mode  '0755'
+  action :create
+  recursive true
+end
+
+#execute "setup-postgres-db-mount-symlink" do
+  #command "rm -rf #{postgres_root}"
+  #action :run
+  #only_if "if [ ! -L #{postgres_root} ]; then exit 0; fi; exit 1;"
+#end
+
+link "setup-postgresq-db-my-symlink" do
+  to '/db/postgresql'
+  target_file postgres_root
+end
 
 execute "init-postgres" do
-  command "if [ ! -d /var/lib/postgresql/8.3/data ]; then\nsu - postgres -c 'initdb -D /var/lib/postgresql/8.3/data'\nfi"
+  command "su - postgres -c 'initdb -D #{postgres_root}/#{postgres_version}/data'"
   action :run
+  only_if "if [ ! -d #{postgres_root} ]; then exit 0; fi; exit 1;"
 end
 
 execute "enable-postgres" do
-  command "rc-update add postgresql-8.3 default"
+  command "rc-update add postgresql-#{postgres_version} default"
   action :run
 end
 
 execute "restart-postgres" do
-  command "/etc/init.d/postgresql-8.3 restart"
+  command "/etc/init.d/postgresql-#{postgres_version} restart"
   action :run
 end
 
 node[:applications].each do |app_name,data|
   user = node[:users].first
   db_name = "#{app_name}_#{node[:environment][:framework_env]}"
-  
+
   execute "create-db-user-#{user[:username]}" do
-    command "`psql -c '\\du' | grep -q '#{user[:username]}'`; if [ $? -eq 1 ]; then\n  psql -c \"create user #{user[:username]} with encrypted password \'#{user[:password]}\'\"\nfi"
+    command "psql -c \"create user #{user[:username]} with encrypted password \'#{user[:password]}\'\""
     action :run
     user 'postgres'
+    only_if "psql -c '\\du' | grep -q '#{user[:username]}'"
   end
-  
+
   execute "create-db-#{db_name}" do
-    command "`psql -c '\\l' | grep -q '#{db_name}'`; if [ $? -eq 1 ]; then\n  createdb #{db_name}\nfi"
+    command "createdb #{db_name}"
     action :run
     user 'postgres'
+    only_if "psql -c '\\l' | grep -q '#{db_name}'"
   end
-  
+
   execute "grant-perms-on-#{db_name}-to-#{user[:username]}" do
     command "psql -c 'grant all on database #{db_name} to #{user[:username]}'"
     action :run
     user 'postgres'
   end
-  
+
   execute "alter-public-schema-owner-on-#{db_name}-to-#{user[:username]}" do
     command "psql #{db_name} -c 'ALTER SCHEMA public OWNER TO #{user[:username]}'"
     action :run
     user 'postgres'
+  end
+
+  template "/data/#{app_name}/shared/config/keep.database.yml" do
+    source "database.yml.erb"
+    owner user[:username]
+    group user[:username]
+    mode 0744
+    variables({
+        :username => user[:username],
+        :app_name => app_name,
+        :db_pass => user[:password]
+    })
   end
 
   template "/data/#{app_name}/shared/config/database.yml" do
@@ -53,6 +89,7 @@ node[:applications].each do |app_name,data|
     group user[:username]
     mode 0744
     variables({
+        :username => user[:username],
         :app_name => app_name,
         :db_pass => user[:password]
     })
