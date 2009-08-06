0dc3a80ac4beb5ab6729fa45916b909834520a4c
Corey Donohoe <atmos@atmos.org>
2009-08-06 01:07:57 -0600

    new stuffs

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,14 +4,9 @@ execute "testing" do
   }
 end
 
-# uncomment if you want to run couchdb recipe
-# require_recipe "couchdb"
-
-# uncomment to turn your instance into an integrity CI server
-#require_recipe "integrity"
-
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
 # require_recipe "mbari-ruby"
 
 # uncomment to enable the postgres database server
+require_recipe 'rubygems'
 require_recipe 'postgres'
